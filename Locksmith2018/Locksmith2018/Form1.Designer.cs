b20fa8e481d8b1d83e9e1f212629b6b934ef5f46
Remus3 <close.benham@gmail.com>
2018-02-24 19:50:59 -0600

    2/24/18 commit night
    2/24 night commit for off duty usage

---
+++ Locksmith2018/Locksmith2018/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Locksmith2018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboMake = new System.Windows.Forms.ComboBox();
+            this.Makelabel = new System.Windows.Forms.Label();
+            this.cboModel = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cboMake
+            // 
+            this.cboMake.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboMake.FormattingEnabled = true;
+            this.cboMake.Location = new System.Drawing.Point(52, 12);
+            this.cboMake.Name = "cboMake";
+            this.cboMake.Size = new System.Drawing.Size(121, 21);
+            this.cboMake.TabIndex = 0;
+            this.cboMake.SelectedIndexChanged += new System.EventHandler(this.cboMake_SelectedIndexChanged);
+            // 
+            // Makelabel
+            // 
+            this.Makelabel.AutoSize = true;
+            this.Makelabel.Location = new System.Drawing.Point(11, 15);
+            this.Makelabel.Name = "Makelabel";
+            this.Makelabel.Size = new System.Drawing.Size(37, 13);
+            this.Makelabel.TabIndex = 1;
+            this.Makelabel.Text = "Make:";
+            // 
+            // cboModel
+            // 
+            this.cboModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboModel.FormattingEnabled = true;
+            this.cboModel.Location = new System.Drawing.Point(52, 51);
+            this.cboModel.Name = "cboModel";
+            this.cboModel.Size = new System.Drawing.Size(121, 21);
+            this.cboModel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 372);
+            this.Controls.Add(this.cboModel);
+            this.Controls.Add(this.Makelabel);
+            this.Controls.Add(this.cboMake);
+            this.Name = "Form1";
+            this.Text = "Locksmith 2018 Armada";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboMake;
+        private System.Windows.Forms.Label Makelabel;
+        private System.Windows.Forms.ComboBox cboModel;
+    }
+}
+
---
+++ Locksmith2018/Locksmith2018/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Locksmith2018
+{
+
+    public partial class Form1 : Form
+    {
+
+        MySqlConnection connection = new MySqlConnection("datasource=sql3.freemysqlhosting.net;port=3306;Initial Catalog='sql3223004';username=sql3223004;password=LmYlAkCZMl");
+        MySqlCommand command;
+        MySqlDataAdapter adapter;
+        DataTable table;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {       
+            string query = "SELECT DISTINCT `Make` FROM `MasterSearch`";
+            fillCombo(cboMake, query, "Make", null);
+            cboMake_SelectedIndexChanged(null, null);
+        }
+        public void fillCombo(ComboBox combo, string query, string displayMember, string valueMember)
+        {
+            command = new MySqlCommand(query, connection);
+            adapter = new MySqlDataAdapter(command);
+            table = new DataTable();
+            adapter.Fill(table);
+            combo.DataSource = table;
+            combo.DisplayMember = displayMember;
+            combo.ValueMember = valueMember;
+        }
+
+        private void cboMake_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           // int Bk = ((ItemData)cboMake.SelectedItem).ID;
+           // string query = "SELECT DISTINCT `Model` FROM `MasterSearch" + Bk + " ";
+            int val;
+            Int32.TryParse(cboMake.SelectedValue.ToString(), out val);
+            string query = "SELECT DISTINCT `Model` FROM `MasterSearch` = " + val + " ";
+            fillCombo(cboModel, query, "Model", null);
+
+        }
+
+    }  public struct ItemData
+        {
+            public int ID;
+            public string EmpName;
+
+
+        public ItemData(int _ID, string _EMName)
+        {
+
+            ID = _ID;
+            EmpName = _EMName;
+
+        }
+
+        }
+      
+}
